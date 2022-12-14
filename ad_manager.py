9a8c5b54aa7a6b40e160f6911b08ee3eb337034a
Billy Dinh <billyxdinh@gmail.com>
2022-12-14 11:05:10 -0500

    add ad manager

---
+++ ad_manager.py
@@ -0,0 +1,35 @@
+import csv
+
+from entities.campaign import Campaign
+from entities.ad_group import AdGroup
+from entities.ad import Ad
+
+
+class AdsManager:
+    def __init__(self):
+        self.campaigns = []
+        self.ad_groups = []
+        self.ads = []
+        self.results = []
+
+    def load_csv(self, path):
+        rows = []
+        with open(path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                rows.append(row)
+
+        return rows
+
+    def export_result(self):
+        pass
+
+    def upload(self, path):
+        self.load_csv(path)
+        print("CSV loaded successfully")
+
+        self.export_result()
+        print("Result exported successfully")
+
+    def print(self, path, entity_type, entity_id):
+        pass
